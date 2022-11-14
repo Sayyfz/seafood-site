d6c5d40d82ebdbf1ca27e9131c892f0694750190
Seif Ashraf <seif_fire2@hotmail.com>
2022-11-14 19:26:49 +0200

    first page done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import About from './components/About';
+import Contact from './components/Contact';
+import Visit from './components/Visit';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Hero />
+      <div className="page">
+        <Hero />
+        <About />
+        <div className="bg">
+          <img src={process.env.PUBLIC_URL + 'Images/img4.jpg'} alt="" />
+          <Contact />
+          <Visit />
+        </div>
+      </div>
     </div>
   );
 }
